787483d2ae99b452ad6dfd188d82ac5e48a208c5
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2014-02-13 09:56:35 -0500

    First commit for linux-system-collectd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -202,7 +202,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Packlinux-system-collectd.tex', u'Pack \\_\\_linux-system-collectd\\_\\_ Documentation',
+  ('index', 'Packlinux-system-collectd.tex', u'Pack linux-system-collectd Documentation',
    u'Thibault Cohen', 'manual'),
 ]
 
