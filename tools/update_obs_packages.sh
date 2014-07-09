7a52831028336ec1202ce21080567ce9d13481ec
aviau <alexandre.viau@savoirfairelinux.com>
2014-07-09 16:16:44 -0400

    Added draft for debian packages

---
+++ tools/update_obs_packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#This is the sfl-shinken-plugins directory
+DIR=$(pwd)
+
+#Checkout the plugins
+#osc co home:ReAzem:sfl-shinken-plugins
+
+for plugin in `ls  | grep plugin-`
+do 
+	#Create the orig source tar
+	date=$(cat $DIR/$plugin/debian/changelog  | grep 'check-' | head -n 1 | awk '{print $2}' | tr -d '()' | cut -d '-' -f 1)
+	tar -czf $DIR/${plugin}_${date}.orig.tar.gz $DIR/$plugin --exclude=${plugin}/debian/* --exclude=${plugin}/.git* --exclude=${plugin}/test
+	#Build the package
+	cd $DIR/$plugin
+	dpkg-buildpackage -us -uc -S	
+
+done
+
+# Add the changes and commit everything
+#osc add home\:ReAzem\:sfl-shinken-plugins/plugin-*/*
+#osc ci home\:ReAzem\:sfl-shinken-plugins/ -m "Update plugins"
