e35c91aa5d66f6ee274157e6568e19e6ddfffebb
Alexandre Viau <alexandre@alexandreviau.net>
2014-07-09 17:07:34 -0400

    typo

--- tools/update_obs_packages.sh
+++ tools/update_obs_packages.sh
@@ -28,6 +28,6 @@ do
 done
 
 # Add the changes and commit everything
-# cd ${DIR}
+#cd ${DIR}
 #osc add home\:ReAzem\:sfl-shinken-plugins/plugin-*/*
 #osc ci home\:ReAzem\:sfl-shinken-plugins/ -m "Update plugins"
