4915c73c0561155ae74bb3a311a9688c3cc04ea4
Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
2014-07-16 14:08:21 -0400

    run-tests.sh: consider new tests runners

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -2,8 +2,15 @@
 set -e
 export GREP_OPTIONS=""
 
-for folder in `ls -d plugin-*/`; do
+for folder in `ls -d plugin-check-*/`; do
     cd $folder
-    ./test/jenkins/jenkins_unit_tests.sh
+    file="./test/jenkins/jenkins_unit_tests.sh"
+    # workaround, as long as we still have these dirty jenkins scripts
+    if [ -e $file ]
+    then
+        $file
+    else
+        ./run_tests.sh
+    fi
     cd ..
 done
