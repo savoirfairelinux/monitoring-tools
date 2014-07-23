ae0c8f58e74008b40536d55d9c52249463c387f0
Matthieu Caneill <matthieucan@users.noreply.github.com>
2014-07-23 16:22:52 -0400

    use $OBS_REPO instead of hardcoded value

--- tools/update-obs-packages.sh
+++ tools/update-obs-packages.sh
@@ -9,7 +9,7 @@ function obs_push {
 	osc co ${OBS_REPO}/$1
 
 	# Check if the OBS orig and the current orig are different
-	rm -rf /tmp/${1}_OBS_ORIG && mkdir /tmp/${1}_OBS_ORIG && tar -xf home:ReAzem:sfl-shinken-plugins/${1}/${1}*.orig.tar.gz -C /tmp/${1}_OBS_ORIG --force-local
+	rm -rf /tmp/${1}_OBS_ORIG && mkdir /tmp/${1}_OBS_ORIG && tar -xf ${OBS_REPO}/${1}/${1}*.orig.tar.gz -C /tmp/${1}_OBS_ORIG --force-local
 	diff -r ${2}/${1}/ /tmp/${1}_OBS_ORIG/${1}/ --exclude=debian --exclude=.git*	
 	
 	#Only update the source has changed
