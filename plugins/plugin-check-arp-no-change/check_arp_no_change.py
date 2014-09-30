eccff3584b540656325dae770e17c467337fe9fb
Grégory Starck <gregory.starck@savoirfairelinux.com>
2014-09-30 18:19:30 -0400

    check_arp_no_change: partially rewrote + begin tests.

--- plugins/plugin-check-arp-no-change/check_arp_no_change.py
+++ plugins/plugin-check-arp-no-change/check_arp_no_change.py
@@ -26,34 +26,116 @@ DEFAULT_IFNAME = "eth0"
 
 import os
 import sys
+import re
 import subprocess
+import warnings
+
+if sys.platform not in ['linux', 'linux2']:
+    warnings.warn('%s is not a supported platform' % sys.platform)
+
+#############################################################################
 
 class ArpRequest:
 
-    def __init__(self, ipaddr, if_name, _arp_cmd="arp -i {if_name} -a {ipaddr}"):
+    ARP_FILE = '/proc/net/arp' # obviously for linux only
+
+    _mac_regex = ':'.join(['\w{2}']*6)
+    _ip_regex = '\.'.join(['\d+']*4)
+
+    _re_proc_net_arp = re.compile(
+        '^'
+        '(?P<ip>'+_ip_regex+')'     # ip
+        '\s+'
+        '[^\s]+'                    # HW type
+        '\s+'
+        '[^\s]+'                    # flags
+        '\s+'
+        '(?P<mac>'+_mac_regex+')'   # MAC/HW address
+        '\s+'
+        '[^\s]+'                    # Mask
+        '\s+'
+        '(?P<device>[^\s]+)'        # Device
+        '$'
+    )
+
+    _re_usr_sbin_arp = re.compile(
+        '^'
+        '[^\s]+'
+        '\s+'
+        '\((?P<ip>'+_ip_regex+')\)'
+        '\s+at\s+'
+        '(?P<mac>'+_mac_regex+')'
+        '\s+'
+        '\[[^\s]+\]'                # HW type
+        '\s+on\s+'
+        '(?P<device>[^\s]+)'        # Device
+        '$'
+    )
+
+    def __init__(self, ipaddr, if_name, _arp_cmd=["arp", "-i", "{if_name}", "-a",  "{ipaddr}"]):
         self.ipaddr = ipaddr
         self.if_name = if_name
         self._arp_cmd = _arp_cmd
+        self._env = os.environ.copy()
+        self._env['LANG'] = 'C'
+
+    def __repr__(self):
+        return '<%s ipaddr=%s if_name=%s />' % (self.__class__.__name__, self.ipaddr, self.if_name)
+
+    def _can_read_arp_file(self):
+        try:
+            with open(self.ARP_FILE):
+                return True
+        except Exception:
+            return False
+
+    def _get_arp_file_content(self):
+        with open(self.ARP_FILE) as fh:
+            fh.readline() # skip header
+            for line in fh:
+                yield line.rstrip()
+
+    def _get_arp_bin_content(self):
+        df = { 'if_name': self.if_name, 'ipaddr': self.ipaddr }
+        cmd_args = tuple(a.format(**df) for a in self._arp_cmd)
+        p = subprocess.Popen(cmd_args, shell=False, stdout=subprocess.PIPE, env=self._env)
+        output, errors = p.communicate()
+        if errors:
+            pass # do what ?
+        return output.split('\n')
+
 
     def request(self):
-        ipaddr = self.ipaddr
-        cmd = self._arp_cmd.format(if_name=self.if_name, ipaddr=self.ipaddr)
-        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        output, errors = p.communicate()
-        # should we check return_code
-        # though not sure every implementation of `arp´ has the same behaviour..
-        if output is not None:
-            if sys.platform in ['linux', 'linux2']:
-                for i in output.split("\n"):
-                    if ipaddr in i:
-                        for j in i.split():
-                            if ":" in j:
-                                return j
+        '''Execute the request.
 
-            elif sys.platform in ['win32']:
-                item = output.split("\n")[-2]
-                if ipaddr in item:
-                    return item.split()[1]
+:return: a dictionnary with following keys:
+    'mac': will contain the MAC in str form ("aa:bb:cc:dd:ee:ff")
+    'ip' : the request ipaddress
+    'device: the device on which the mac was found.'''
+        if self._can_read_arp_file():
+            regex, content = self._re_proc_net_arp, self._get_arp_file_content()
+        else:
+            content = self._get_arp_bin_content()
+            if sys.platform in ['linux', 'linux2']:
+                regex = self._re_usr_sbin_arp
+            else:
+                raise RuntimeError('platform %s not currently supported.' % sys.platform)
+            # TODO: elif sys.platform in ['win32']:
+            #   pass
+
+        for line in content:
+            match = regex.match(line)
+            if not match:
+                continue
+            gd = match.groupdict()
+            if gd['ip'] != self.ipaddr:
+                continue
+            if ((self.if_name and gd['device'] == self.if_name)
+                or not self.if_name
+            ):
+                return gd
+
+
 
 
 #############################################################################
@@ -74,7 +156,7 @@ class Plugin(BasePlugin):
         ('m', 'mac', 'The MAC address the host should have', True),
         #('c', 'critical', "If ARP doesn't match then return a warning ; default is to return a warning.", False),
         #('w', 'warn-on-not-found', "If ARP isn't found (timeout) then force a warning return.", False),
-        ('i', 'if_name', 'Explicitly specify the interface name on which to look for the ARP address. default='+DEFAULT_IFNAME, False),
+        ('i', 'if_name', 'Explicitly specify the interface name on which to look for the ARP address. default='+DEFAULT_IFNAME, True),
         ('p', 'ping_before', 'Do you want me to ping the host before reading its ARP address ?', False)
     ]
     
@@ -92,8 +174,9 @@ class Plugin(BasePlugin):
         if ping_before:
             os.popen('ping -W 2 -c 1 %s' % host)
         r = ArpRequest(host, if_name=if_name)
-        mac_read = r.request()
-        if mac_read:
+        data = r.request()
+        if data:
+            mac_read = data.get('mac', '')
             if mac != mac_read:
                 self.critical("MAC address for host %s = %s doesn't match "
                               "the expected one (%s)" % (host, mac_read, mac))
--- plugins/plugin-check-arp-no-change/test_check_arp_no_change.py
+++ plugins/plugin-check-arp-no-change/test_check_arp_no_change.py
@@ -14,10 +14,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Copyright (C) 2014, Grégory Starck <gregory.starck@savoirfairelinux.com>
+import unittest
+from unittest.case import skip
 
+import check_arp_no_change
 from check_arp_no_change import Plugin
 
 from shinkenplugins import TestPlugin
+
 
 class Test(TestPlugin):
     def test_version(self):
@@ -38,3 +42,49 @@ class Test(TestPlugin):
     #              'regex to check against the output')
     # You can also add debug=True, to get useful information
     # to debug your plugins
+
+
+# removed header line from this fake arp data:
+# (Address                  HWtype  HWaddress           Flags Mask            Iface)
+FAKE_ARP_DATA = '''\
+172.17.0.12      0x1         0x0         e6:9e:ed:bc:b6:67     *        eth1
+192.168.50.1     0x1         0x2         00:21:a1:39:96:40     *        eth0
+172.17.0.15      0x1         0x2         2e:d6:1b:d0:f6:e0     *        wlan0
+172.17.0.8       0x1         0x2         72:7d:5f:ca:03:5a     *        wlan1
+172.17.0.11      0x1         0x2         36:57:18:09:4e:09     *        eth0
+172.17.0.14      0x1         0x2         1e:0e:8e:6b:5d:32     *        eth0
+172.17.0.4       0x1         0x2         96:5d:d9:ee:54:05     *        eth0'''
+
+
+class Test_ARP_mock(TestPlugin):
+
+    def make_ArpRequest(self, content):
+        class ArpRequest(check_arp_no_change.ArpRequest):
+            def _get_arp_file_content(self):
+                for line in content:
+                    yield line
+        return ArpRequest
+
+    def setUp(self):
+        check_arp_no_change.ArpRequest = self.make_ArpRequest(FAKE_ARP_DATA.split('\n'))
+
+    def tearDown(self):
+        pass
+
+
+    def test_unknown_interface(self):
+        self.execute(Plugin, ('-H', '172.17.0.12', '-i', 'no_such_if', '-m', '00:21:a1:39:96:40'),
+                     1, 'ARP address not found')
+
+    def test_no_arp(self):
+        self.execute(Plugin, ('-H', '172.17.0.8', '-i', 'eth0', '-m', '00:21:a1:39:96:40'),
+                     1, 'ARP address not found')
+
+    def test_mac_changed(self):
+        self.execute(Plugin, ('-H', '172.17.0.12', '-i', 'eth1', '-m', '00:21:a1:39:96:41'),
+                     2, "MAC address for host .* doesn't match the expected one")
+
+    def test_ok(self):
+        self.execute(Plugin, ('-H', '172.17.0.12', '-i', 'eth1', '-m', 'e6:9e:ed:bc:b6:67'),
+                     0, '^OK given MAC address corresponds to host')
+
