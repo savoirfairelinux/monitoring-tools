3e1ae4812d09f4fe8d5512e6c262009fda6919fe
aviau <alexandre.viau@savoirfairelinux.com>
2014-11-22 11:03:13 -0500

    Use pip wheel for faster builds

--- plugins/plugin-check-postgresql-lag/run_tests.sh
+++ plugins/plugin-check-postgresql-lag/run_tests.sh
@@ -3,7 +3,7 @@
 #rm -rf env
 #virtualenv env
 #source env/bin/activate
-#pip install -I nose
-#[ -f requirements.tests.txt ] && pip install -r requirements.tests.txt
-#pip install -r requirements.txt
+#pip install -f /tmp/wheelhouse -I nose
+#[ -f requirements.tests.txt ] && pip install -f /tmp/wheelhouse -r requirements.tests.txt
+#pip install -f /tmp/wheelhouse -r requirements.txt
 #nosetests && rm -rf env
