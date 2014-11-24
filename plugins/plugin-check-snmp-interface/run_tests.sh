80c878808b76b5ec029e946e6c1426265f2bd3e2
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2014-11-24 15:18:44 -0500

    Fix test for plugin-check-snmp-interface

--- plugins/plugin-check-snmp-interface/run_tests.sh
+++ plugins/plugin-check-snmp-interface/run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#rm -rf env
-#virtualenv env
-#source env/bin/activate
-#pip install --use-wheel --find-links=/tmp/wheelhouse -I nose
-#[ -f requirements.tests.txt ] && pip install --use-wheel --find-links=/tmp/wheelhouse -r requirements.tests.txt
-#pip install --use-wheel --find-links=/tmp/wheelhouse -r requirements.txt
-#nosetests && rm -rf env
+rm -rf env
+virtualenv env
+source env/bin/activate
+pip install --use-wheel --find-links=file:///tmp/wheelhouse -I nose
+[ -f requirements.tests.txt ] && pip install --use-wheel --find-links=file:///tmp/wheelhouse -r requirements.tests.txt
+pip install --use-wheel --find-links=file:///tmp/wheelhouse -r requirements.txt
+nosetests && rm -rf env
