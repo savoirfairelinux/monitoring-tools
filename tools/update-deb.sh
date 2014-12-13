6020ffb81e8695ac4f07fc796c4c446acaa7e90c
Thibault Cohen <titilambert@gmail.com>
2014-12-13 03:31:47 -0500

    Clean packaging and obs scripts

--- tools/update-deb.sh
+++ tools/update-deb.sh
@@ -5,59 +5,111 @@ export GREP_OPTIONS=""
 
 set -e
 
+package_name=$1
+
+# Colors
+red='\e[0;31m'
+green='\e[0;32m'
+NC='\e[0m' # No Color
+
 # -S builds only the source package, the binary one is done by OpenBuildService
 # --ignore-bad-version skips the date check, because the files can be more recent
 # than the last debian/changelog entry
 BUILD_PACKAGE="dpkg-buildpackage -us -uc -S --source-option=-Zgzip --source-option=--ignore-bad-version"
 
-# libs packages
-cd libs
-for lib in `ls -d */ | tr -d '/'`
-do
+# create build-are folder
+BASEDIR=$(dirname $(readlink -f "$0"))/..
+BUILD_AREA=$BASEDIR/build-area
+
+cd $BASEDIR
+mkdir -p $BUILD_AREA/libs
+mkdir -p $BUILD_AREA/plugins
+mkdir -p $BUILD_AREA/packs
+
+
+function build_package {
+    # lib, plugin, pack
+    package_type=$1
+    # package name
+    package=$2
+
+    echo
+    echo "============================================================="
+    echo "             Prepare $package"
+    echo "============================================================="
+
     # We extract the last version from the changelog
-    version=$(cat $lib/debian/changelog  | grep 'urgency=' | head -n 1 | awk '{print $2}' | tr -d '()' | cut -d '-' -f 1)
+    cd $BASEDIR/${package_type}s
+    version=$(cat ${package}/debian/changelog  | grep 'urgency=' | head -n 1 | awk '{print $2}' | tr -d '()' | cut -d '-' -f 1)
+    rm -rf $BUILD_AREA/${package_type}s/${package}
+    cp -r ${package} $BUILD_AREA/${package_type}s
+    cd $BUILD_AREA/${package_type}s
+    tar -czf ${package}_${version}.orig.tar.gz ${package}/ --exclude=${package}/debian* --exclude=${package}/.git*
 
-    # We create the upstream source, foo.orig.tar.gz
-    tar -czf ${lib}_${version}.orig.tar.gz $lib/ --exclude=${lib}/debian/* --exclude=${lib}/.git*
+    cd ${package}
+    $BUILD_PACKAGE > ../build-${package}.report 2>&1
+    if [[ $? -eq 0 ]]
+    then
+        echo -e "${green}Build OK${NC}"
+    else
+        echo -e "${red}Build ERROR. Please look here: $BUILD_AREA/${package_type}s/build-${package}.report${NC}"
+        cat $BUILD_AREA/${package_type}s/build-${package}.report${NC}
+    fi
+    cd $BASEDIR
+}
 
-    # And let's build the source package
-    cd $lib
-    $BUILD_PACKAGE || true
-    cd ..
-done
-cd ..
+
+
+
+# libs packages
+if [ "$package_name" != "" ]
+then
+    if [ -d $BASEDIR/libs/$package_name ]
+    then
+        build_package lib $package_name
+    else
+        echo -e "\n${red}${package_name} is NOT a lib${NC}"
+    fi
+else
+    cd libs
+    for lib in `ls -d */ | tr -d '/'`
+    do
+        build_package lib $lib
+    done
+fi
 
 # plugin packages
-cd plugins
-for plugin in `ls -d */ | tr -d '/'`
-do 
-    # We extract the last version from the changelog
-    version=$(cat $plugin/debian/changelog  | grep 'check-' | head -n 1 | awk '{print $2}' | tr -d '()' | cut -d '-' -f 1)
-    
-    # We create the upstream source, foo.orig.tar.gz
-    tar -czf ${plugin}_${version}.orig.tar.gz $plugin/ --exclude=${plugin}/debian/* --exclude=${plugin}/.git*
-    
-    # And let's build the source package
-    cd $plugin
-    $BUILD_PACKAGE || true
-    cd ..
-done
-cd ..
+if [ "$package_name" != "" ]
+then
+    if [ -d $BASEDIR/plugins/$package_name ]
+    then
+        build_package plugin $package_name
+    else
+        echo -e "\n${red}${package_name} is NOT a plugin${NC}"
+    fi
+else
+    cd plugins
+    for plugin in `ls -d */ | tr -d '/'`
+    do
+        build_package plugin $plugin
+    done
+fi
 
 # pack packages
-cd packs
-for pack in `ls -d */ | tr -d '/'`
-do
-    # Extract last version number
-    version=$(cat $pack/debian/changelog | grep 'pack-' | awk '{print $2}' | tr -d '()' | cut -d '-' -f 1)
+if [ "$package_name" != "" ]
+then
+    if [ -d $BASEDIR/packs/$package_name ]
+    then
+        build_package pack $package_name
+    else
+        echo -e "\n${red}${package_name} is NOT a pack${NC}"
+    fi
+else
+    cd packs
+    for pack in `ls -d */ | tr -d '/'`
+    do
+        build_package pack $pack
+    done
+fi
 
-    # Create the "upstream source" archive
-    # If the directory contains a collectd directory: exclude it
-    tar -czf ${pack}_${version}.orig.tar.gz $pack/ --exclude=${pack}/debian/* --exclude=${pack}/.git*
-
-    # Build the source package
-    cd $pack
-    dpkg-buildpackage -us -uc -S --source-option=-Zgzip --source-option=--ignore-bad-version --source-option=-Icollectd || true
-    cd ..
-
-done
+exit 0
--- tools/update-obs-packages.sh
+++ tools/update-obs-packages.sh
@@ -1,33 +1,75 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $(readlink -f "$0"))/..
+BUILD_AREA=$BASEDIR/build-area
+
+
+package_name=$1
+
+# Colors
+red='\e[0;31m'
+green='\e[1;32m'
+lightgreen='\e[0;32m'
+blue='\e[1;34m'
+yellow='\e[1;33m'
+NC='\e[0m' # No Color
+
 # first arg: name of the obs package
 # second arg: path of the source files
 function obs_push {
+    # lib, plugin, pack
+    package_type=$1
+    # package name
+    package=$2
+
+    echo
+    echo "============================================================="
+    echo "             Prepare ${package}"
+    echo "============================================================="
+
+
     # Checkout the package
-    osc co ${OBS_REPO}/$1
+    echo -e "${blue}Checkout OBS repo${NC}"
+    cd ${BASEDIR}/obs.tmp
+    rm -rf ${OBS_REPO}/${package}
+    osc co ${OBS_REPO}/${package}
 
     # Check if the OBS orig and the current orig are different
-    rm -rf /tmp/${1}_OBS_ORIG && mkdir /tmp/${1}_OBS_ORIG && tar -xf ${OBS_REPO}/${1}/${1}*.orig.tar.gz -C /tmp/${1}_OBS_ORIG --force-local
-    diff -r ${2}/${1}/ /tmp/${1}_OBS_ORIG/${1}/ --exclude=.git*
+    echo -e "${blue}Decompress OBS ${archive_name} archive${NC}"
+    rm -rf /tmp/${package}_OBS_ORIG && mkdir -p /tmp/${package}_OBS_ORIG
+    tar -xf ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/${package}*.orig.tar.gz -C /tmp/${package}_OBS_ORIG --force-local
+    tar -xf ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/${package}*.debian.tar.gz -C /tmp/${package}_OBS_ORIG/${package} --force-local
+
+    # Get differences from obs and local dir
+    echo -e "${blue}Compare ${archive_name} archives${NC}"
+    diff -r ${BUILD_AREA}/${package_type}s/${package}/ /tmp/${package}_OBS_ORIG/${package}/ --exclude=.git*
+
 
     #Only update the source has changed
     if [ $? -ne 0 ]; then
-        echo Source has changed, uploading to obs...
+        echo -e "${yellow}Source has changed, uploading to obs...${NC}"
 
         # Remove the old files
-        rm ${DIR}/${OBS_REPO}/$1/*
+        rm ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/*
 
         # Copy the new files
-        mv $2/$1*.tar.gz ${DIR}/${OBS_REPO}/$1/
-        mv $2/$1*.dsc ${DIR}/${OBS_REPO}/$1/
-        mv $2/$1*.changes ${DIR}/${OBS_REPO}/$1/
-        mv $2/$1*.diff.gz ${DIR}/${OBS_REPO}/$1/
+        cp ${BUILD_AREA}/${package_type}s/${package}*.tar.gz ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/
+        cp ${BUILD_AREA}/${package_type}s/${package}*.dsc ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/
+        cp ${BUILD_AREA}/${package_type}s/${package}*.changes ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/
+        cp ${BUILD_AREA}/${package_type}s/${package}*.debian.tar.gz ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/
 
         # Add the changes and commit
-        osc addremove ${DIR}/${OBS_REPO}/$1/*
-        osc ci ${DIR}/${OBS_REPO}/$1 -m "Updated ${1}"
+        echo -e "${blue}SENDING to OBS${NC}"
+        osc addremove ${BASEDIR}/obs.tmp/${OBS_REPO}/${package}/*
+        osc commit ${BASEDIR}/obs.tmp/${OBS_REPO}/${package} -m "Updated ${package}"
+        if [[ $? -eq 0 ]]
+        then
+            echo -e "${green}sent to OBS${NC}"
+        else
+            echo -e "${red}ERROR: NOT sent to OBS${NC}"
+        fi
     else
-        echo Skipping OBS upload...
+        echo -e "${lightgreen}Sources inchanged. Skipping OBS upload...${NC}"
     fi
 }
 
@@ -38,21 +80,55 @@ DIR=$(pwd)
 # Open Build Service repository
 OBS_REPO=home:sfl-monitoring:monitoring-tools
 
+mkdir -p ${BASEDIR}/obs.tmp && cd ${BASEDIR}/obs.tmp
 
 # libraries
-for lib in `(cd libs && ls -d */ | tr -d '/')`
-do
-    obs_push $lib libs
-done
+if [ "$package_name" != "" ]
+then
+    if [ -d ${BUILD_AREA}/libs/$package_name ]
+    then
+        obs_push lib $package_name
+    else
+        echo -e "\n${red}${package_name} is NOT built${NC}"
+    fi
+
+else
+    for lib in `(cd ${BASEDIR}/libs && ls -d */ | tr -d '/')`
+    do
+        obs_push lib $lib
+    done
+fi
 
 # plugins
-for plugin in `(cd plugins && ls -d */ | tr -d '/')`
-do
-    obs_push $plugin plugins
-done
+if [ "$package_name" != "" ]
+then
+    if [ -d ${BUILD_AREA}/plugins/$package_name ]
+    then
+        obs_push plugin $package_name
+    else
+        echo -e "\n${red}${package_name} is NOT built${NC}"
+    fi
+
+else
+    for plugin in `(cd ${BASEDIR}/plugins && ls -d */ | tr -d '/')`
+    do
+        obs_push plugin $plugin
+    done
+fi
 
 # packs
-for pack in `(cd packs && ls -d */ | tr -d '/')`
-do
-    obs_push $pack packs
-done
+if [ "$package_name" != "" ]
+then
+    if [ -d ${BUILD_AREA}/packs/$package_name ]
+    then
+        obs_push pack $package_name
+    else
+        echo -e "\n${red}${package_name} is NOT built${NC}"
+    fi
+
+else
+    for pack in `(cd ${BASEDIR}/packs && ls -d */ | tr -d '/')`
+    do
+        obs_push pack $pack
+    done
+fi
