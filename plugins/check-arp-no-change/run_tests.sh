10e721e4270ff6b61bce110f60e26708132b8110
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2015-01-30 10:31:30 -0500

    Try to fix check_arp_no_change tests

--- plugins/check-arp-no-change/run_tests.sh
+++ plugins/check-arp-no-change/run_tests.sh
@@ -5,5 +5,6 @@ virtualenv env
 source env/bin/activate
 pip install --use-wheel --find-links=file:///tmp/wheelhouse -I nose
 [ -f requirements.tests.txt ] && pip install --use-wheel --find-links=file:///tmp/wheelhouse -r requirements.tests.txt
+pip install mock
 pip install --use-wheel --find-links=file:///tmp/wheelhouse -r requirements.txt
 nosetests && rm -rf env
