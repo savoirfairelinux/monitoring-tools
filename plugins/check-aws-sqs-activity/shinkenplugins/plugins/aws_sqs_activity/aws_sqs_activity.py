dc23305f9d571c73ac570b695970db02c576d627
Sebastien Coavoux <sebastien.coavoux@savoirfairelinux.com>
2015-02-20 09:09:53 -0500

    Fix : Remove import loop

--- plugins/check-aws-sqs-activity/shinkenplugins/plugins/aws_sqs_activity/aws_sqs_activity.py
+++ plugins/check-aws-sqs-activity/shinkenplugins/plugins/aws_sqs_activity/aws_sqs_activity.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from shinkenplugins.plugins.aws_sqs_activity import main
-
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- plugins/check-bixi-montreal/shinkenplugins/plugins/bixi_montreal/bixi_montreal.py
+++ plugins/check-bixi-montreal/shinkenplugins/plugins/bixi_montreal/bixi_montreal.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from shinkenplugins.plugins.bixi_montreal import main
-
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
