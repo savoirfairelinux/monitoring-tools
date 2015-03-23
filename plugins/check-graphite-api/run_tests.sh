b92c84adcd701ebcd3a86e8ba02ec20c82419938
Grégory Starck <gregory.starck@savoirfairelinux.com>
2015-03-23 16:43:18 -0400

    Made links to plugins/run_tests.sh

--- plugins/check-graphite-api/run_tests.sh
+++ plugins/check-graphite-api/run_tests.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-wheel_house="/tmp/wheelhouse"
-
-rm -rf env
-virtualenv env
-source env/bin/activate
-
-pip install --upgrade pip nose setuptools wheel
-
-pip wheel --find-links=${wheel_house} --wheel-dir=${wheel_house} .
-
-pip install --use-wheel --find-links=${wheel_house} -e .[test]
-
-(cd tests && nosetests) && rm -rf env
+../run_tests.sh
