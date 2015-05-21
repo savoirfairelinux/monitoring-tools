2aa1ae6443841cf4763197a37bdd2dbc23431b94
Grégory Starck <gregory.starck@savoirfairelinux.com>
2015-05-21 17:55:46 -0400

    Enh: tools/run-tests.sh: skip libs with no run_tests.sh
    but print them at the end.

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -14,6 +14,9 @@ $w pysnmp
 $w nose
 $w shinkenplugins
 
+
+not_tested=""
+
 # Plugins
 cd plugins
 for plugin in `ls -d */`; do
@@ -36,7 +39,25 @@ cd libs
 for lib in `ls -d */`
 do
     cd $lib
-    ./run_tests.sh
+    if test -x run_test.sh
+    then
+        ./run_tests.sh
+    else
+        echo "WARNING: $lib not tested"
+        not_tested="$not_tested\n$lib"
+    fi
     cd ..
 done
 cd ..
+
+
+if test "$not_tested"
+then
+    echo -e $(cat << END
+===============================================
+WARNING WARNING following libs were NOT tested:
+$not_tested
+===============================================
+END
+)
+fi
