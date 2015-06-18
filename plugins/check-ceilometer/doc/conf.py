c1c4369fd67f49377f6d2a0392654815ee1a7323
Vincent Fournier <vince@ntfournier.com>
2015-06-18 18:18:04 +0000

    Package for plugin check_ceilometer

--- plugins/check-ceilometer/doc/conf.py
+++ plugins/check-ceilometer/doc/conf.py
@@ -19,7 +19,7 @@ project = u'plugin-check_ceilometer'
 copyright = u'2015, Savoir-faire Linux <supervision@savoirfairelinux.com>'
 
 # The short X.Y version.
-version = '2015.2.19.16.37'
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags.
 release = '1'
 
@@ -194,8 +194,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('plugin-check_ceilometer', 'monitoring-packs-sfl-generic-host', u'plugin-check_ceilometer Documentation',
-     [u'Savoir-faire Linux'], 7)
+    ('plugin-check_ceilometer', 'check_ceilometer', u'plugin-check_ceilometer Documentation',
+     [u'Savoir-faire Linux'], 1)
 ]
 
 # If true, show URL addresses after external links.
